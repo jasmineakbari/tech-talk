1073d9764707f88743f83d9c39c4a602ca1a7c00
unknown <jasmineakbari@yahoo.com>
2021-02-07 15:47:34 -0500

    corrected add post and comment errors added extra styling

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,14 +1,25 @@
-async function logout() {
-    const response = await fetch('/api/users/logout', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
+async function editFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
   }
-  
-document.querySelector('#logout').addEventListener('click', logout);
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
